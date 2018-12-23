e25e7dc2ed8eb2a38b7433c943643b4e44a27578
weiersyuan <weiersyuan@tencent.com>
2018-12-23 23:53:58 +0800

    增加删除功能

--- app/src/main/java/com/test/voicecontroller/VoiceController.kt
+++ app/src/main/java/com/test/voicecontroller/VoiceController.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.graphics.Color
 import android.graphics.Point
 import android.graphics.Rect
+import android.graphics.drawable.GradientDrawable
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.support.v7.widget.VoiceLinearSnapHelper
@@ -14,6 +15,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.RelativeLayout
+import android.widget.Toast
 import com.test.voicecontroller.data.BaseVoiceItem
 import com.test.voicecontroller.data.VoiceSegment
 import com.test.voicecontroller.data.VoiceSeperator
@@ -23,6 +25,7 @@ class VoiceController @JvmOverloads constructor(
 ) : RelativeLayout(context, attrs, defStyleAttr), View.OnClickListener {
 
     private var playIv: ImageView
+    private var centerLine: View
     private var deleteSegmentIv: ImageView
     private var recyclerView: RecyclerView
 
@@ -30,26 +33,34 @@ class VoiceController @JvmOverloads constructor(
 
     private var snapHelper: VoiceLinearSnapHelper
 
-    private val adapter = VoiceAdapter()
+    private val adapter :VoiceAdapter
     private var headerAndFooterRecyclerViewAdapter: HeaderAndFooterRecyclerViewAdapter
 
     private var centerPosition = Point(0,0)
+    private val centerLineConerSize = ConvertUtil.dp2px(2f)
 
     init {
         LayoutInflater.from(context).inflate(R.layout.voice_controller_layout, this, true)
         playIv = findViewById(R.id.play)
+        centerLine = findViewById(R.id.centerLine)
         deleteSegmentIv = findViewById(R.id.deleteSegment)
         recyclerView = findViewById(R.id.recyclerView)
         layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
         recyclerView.layoutManager = layoutManager
+        recyclerView.itemAnimator = null
 
+        centerLine.background = GradientDrawable().apply {
+            setColor(Color.RED)
+            cornerRadii = floatArrayOf(centerLineConerSize, centerLineConerSize, centerLineConerSize, centerLineConerSize, centerLineConerSize, centerLineConerSize, centerLineConerSize, centerLineConerSize)
+        }
         snapHelper = VoiceLinearSnapHelper()
         snapHelper.attachToRecyclerView(recyclerView)
-
+        adapter = VoiceAdapter(recyclerView)
         headerAndFooterRecyclerViewAdapter = HeaderAndFooterRecyclerViewAdapter(adapter)
         headerAndFooterRecyclerViewAdapter.addHeaderView(getEmptyView(context))
         headerAndFooterRecyclerViewAdapter.addFooterView(getEmptyView(context))
         recyclerView.adapter = headerAndFooterRecyclerViewAdapter
+        deleteSegmentIv.setOnClickListener(this)
     }
 
     /**
@@ -61,7 +72,7 @@ class VoiceController @JvmOverloads constructor(
             layoutManager.getPosition(view)
         } else {
             -1
-        }
+        } - headerAndFooterRecyclerViewAdapter.headerViewsCount
     }
 
     /**
@@ -97,7 +108,7 @@ class VoiceController @JvmOverloads constructor(
         }
     }
 
-    class VoiceAdapter: RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    class VoiceAdapter(val recyclerView: RecyclerView): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
         companion object {
             private const val VIEW_TYPE_VOICE_SEGMENT = 0
             private const val VIEW_TYPE_VOICE_SEPERATOR = 1
@@ -155,14 +166,48 @@ class VoiceController @JvmOverloads constructor(
             //
             if (cursorPosition >= 0 && cursorPosition < data.size) {
                 val itemData = data[cursorPosition]
-//                if (segment.selected) {
-//                    // 删除
-//                    data.removeAt(cursorPosition)
-//                    notifyItemRemoved(cursorPosition)
-//                } else {
-//                    segment.selected = true
-//                    notifyItemChanged(cursorPosition)
-//                }
+                if (itemData is VoiceSeperator) {
+                    if (itemData.type == VoiceSeperator.TYPE_START) {
+                        Toast.makeText(BaseApplication.application, "该位置无法删除", Toast.LENGTH_SHORT).show()
+                    } else {
+                        // 删除数据源直到遇到上一个 普通分隔符或者起始分隔符
+                        val pendingDeleteIndexList = ArrayList<Int>()
+                        val startItemData = data[cursorPosition]
+                        pendingDeleteIndexList.add(cursorPosition)
+                        for (i in cursorPosition-1 downTo 0) {
+                            val currentItemData = data[i]
+                            pendingDeleteIndexList.add(i)
+                            if (currentItemData is VoiceSeperator) {
+                                if (currentItemData.type == VoiceSeperator.TYPE_START) {
+                                    // 遇到上一个start
+                                    if (startItemData is VoiceSeperator && startItemData.type == VoiceSeperator.TYPE_NORMAL) {
+                                        pendingDeleteIndexList.removeAt(pendingDeleteIndexList.size - 1)
+                                    }
+                                    break
+                                } else if (currentItemData.type == VoiceSeperator.TYPE_NORMAL) {
+                                    pendingDeleteIndexList.removeAt(0)
+                                    break
+                                }
+                            }
+                        }
+                        //
+                        Log.d("weiers", "delete index is $pendingDeleteIndexList")
+                        val from = pendingDeleteIndexList[pendingDeleteIndexList.size - 1]
+                        for (i in pendingDeleteIndexList) {// 逆序
+                            data.removeAt(i)
+                        }
+                        notifyItemRangeRemoved(from, pendingDeleteIndexList.size)
+                        // 滚动
+                        var totalScrollWidth = 0
+                        for (i in pendingDeleteIndexList) {
+                            recyclerView.findViewHolderForLayoutPosition(i)?.let {
+                                totalScrollWidth += it.itemView.width
+                            }
+                        }
+                        Log.d("weiers", "totalScrollWidth=$totalScrollWidth")
+                        recyclerView.smoothScrollBy(-totalScrollWidth, 0)
+                    }
+                }
             } else {
                 Log.d("weiers", "onDeleteClick, cursorPosition error, cursorPosition is $cursorPosition")
             }
